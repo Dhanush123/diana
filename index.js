f3f71eb7b558bb75b928a90274b83e72d788be58
Dhanush123 <dhanush.patel@ymail.com>
2017-06-11 11:11:59 -0700

    uhin[iun
    aWezsdfc

--- index.js
+++ index.js
@@ -131,8 +131,8 @@ function getQuizlets(usertopic, mainRes) {
             result = result.substring(0, 160);
             console.log("Skype: " + result);
             return mainRes.json({
-              "skype": {
-                "text": result
+              skype: {
+                text: result
               }
             });
           }
