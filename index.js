c32d51246b08ff75cfd352f65d201044b78b5d74
Dhanush123 <dhanush.patel@ymail.com>
2017-06-11 02:48:36 -0700

    sadsad
    sdasdasd

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const request = require('request');
 const server = express();
 server.use(bodyParser.json());
 
+var cardsSend = [];
+
 server.post('/hook', function (req, res) {
   console.log('hook request');
   try {
@@ -18,10 +20,20 @@ server.post('/hook', function (req, res) {
               getUserTopic(requestBody,function(result) {
                 console.log('result: ', "test response1");
                 return res.json({
-                  speech: "test response1",
-                  displayText: "test response1",
-                  source: 'dhanush-quakey'
-                });
+                      attachment: {
+                          type: "template",
+                          payload: {
+                              template_type: "generic",
+                              elements: cardsSend
+                            }
+                          }
+                        });
+
+                // return res.json({
+                //   speech: "test response1",
+                //   displayText: "test response1",
+                //   source: 'dhanush-diana'
+                // });
               });
             }
           }
@@ -40,8 +52,46 @@ server.post('/hook', function (req, res) {
 
 function getUserTopic(requestBody, callback) {
   console.log('requestBody: ' + JSON.stringify(requestBody));
-      callback();
+  getQuizlets(requestBody.result.parameters.usertopic,callback);
 }
+
+function getQuizlets(usertopic, clbk){
+  var options = {
+    url: "https://api.quizlet.com/2.0/search/sets?q="+usertopic+"&client_id=DZH2jBMBKx"
+  };
+  function callback(err,res,body){
+    if(!err && body){
+      var nSets = body.sets.length;
+      var randNum = Math.floor(Math.random() * nSets);
+      var chosenSetID = body.sets[randNum].id;
+
+      var options = {
+        url: "https://api.quizlet.com/2.0/sets/"+chosenSetID+"?client_id=DZH2jBMBKx&whitespace=1"
+      };
+      function callback2(err2,res2,body2){
+        if(!err2 && body2){
+        //  var termNames = [];
+        //  var defs = [];
+         var nCards = terms.length > 10 ? 10 : terms.length;
+         for(var i = 0; i < nCards; i++){
+           var cardObj = {
+                title: "",
+                subtitle: "",
+                "type": 1
+           };
+           cardObj.title = terms[i].term;
+           cardObj.subtitle = terms[i].definition;
+         }
+         clbk();
+        }
+      }
+      request(options2,callback2);
+    }
+  }
+  request(options,callback);
+}
+
+
 
 //for reference: http://stackoverflow.com/questions/37960857/how-to-show-personalized-welcome-message-in-facebook-messenger?answertab=active#tab-top
 function createGreetingApi(data) {
